74bed455c8616c9c2ed858476de96c409dc4f359
shaivpidadi <shaivpidadi@gmail.com>
2020-12-08 16:55:19 +0530

    Added Rimble UI With dummy buttons

--- src/views/Login/Login.js
+++ src/views/Login/Login.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import { MetaMaskButton, Button } from 'rimble-ui';
+
+import './Login.css';
 
 const Login = () => (
-    <div>
-        Login
+  <div className="LoginContainerWrapper">
+    <div className="LoginButtonWrapper">
+      <MetaMaskButton className="LoginButtonMargin" onClick={() => alert('Metamask Clicked')}>Connect with MetaMask</MetaMaskButton>
+      <Button className="LoginButtonMargin" onClick={() => alert('WalletConnect Clicked')}> Connect WalletConnect</Button>
     </div>
+  </div>
 )
 
 export default Login;
