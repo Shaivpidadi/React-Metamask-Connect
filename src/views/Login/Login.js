285d7184e3aa0615b6dd6495ca7d22b9b6acc683
shaivpidadi <shaivpidadi@gmail.com>
2020-12-08 17:54:08 +0530

    Added Metamask onboarding

--- src/views/Login/Login.js
+++ src/views/Login/Login.js
@@ -1,25 +1,58 @@
 import React, { useEffect, useState } from 'react';
 import { MetaMaskButton, Button } from 'rimble-ui';
+import MetaMaskOnboarding from '@metamask/onboarding';
 
 import './Login.css';
-import { isMetaMaskInstalled as checkMetamask } from '../../utils';
+
+const ONBOARD_TEXT = 'Click to install MetaMask!';
+const CONNECT_TEXT = 'Connect';
+const CONNECTED_TEXT = 'Connected';
 
 const Login = () => {
 
-  const [isMetaMaskInstalled, updateIsMetamaskInstalled] = useState(false);
+  const [buttonText, setButtonText] = React.useState(ONBOARD_TEXT);
+  const [isDisabled, setDisabled] = React.useState(false);
+  const [accounts, setAccounts] = React.useState([]);
+  const onboarding = React.useRef();
+
 
   useEffect(() => {
-    if (checkMetamask())
-      updateIsMetamaskInstalled(true);
-    else
-      updateIsMetamaskInstalled(false);
+    if (!onboarding.current) {
+      onboarding.current = new MetaMaskOnboarding();
+    }
   }, []);
+
+  useEffect(() => {
+    if (MetaMaskOnboarding.isMetaMaskInstalled()) {
+      if (accounts.length > 0) {
+        setButtonText(CONNECTED_TEXT);
+        setDisabled(true);
+        onboarding.current.stopOnboarding();
+      } else {
+        setButtonText(CONNECT_TEXT);
+        setDisabled(false);
+      }
+    }
+  }, [accounts]);
+
+  const handleMetamaskClick = () => {
+    if (MetaMaskOnboarding.isMetaMaskInstalled()) {
+      window.ethereum
+        .request({ method: 'eth_requestAccounts' })
+        .then((newAccounts) => {
+          console.log({ newAccounts })
+          setAccounts(newAccounts)
+        });
+    } else {
+      onboarding.current.startOnboarding();
+    }
+  }
 
   return (
     <div className="LoginContainerWrapper">
       <div className="LoginButtonWrapper">
-        <MetaMaskButton className="LoginButtonMargin" onClick={() => alert('Metamask Clicked')}>
-          {isMetaMaskInstalled ? 'Connect with MetaMask' : 'Install Metamask'}
+        <MetaMaskButton className="LoginButtonMargin" isDisabled={isDisabled} onClick={() => handleMetamaskClick()}>
+          {buttonText}
         </MetaMaskButton>
         <Button className="LoginButtonMargin" onClick={() => alert('WalletConnect Clicked')}> Connect WalletConnect</Button>
       </div>
